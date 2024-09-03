9b67ce3e38a4ab7147abc54c276d9afe88fe69d7
Jds Dofp <jdsdofp@gmail.com>
2024-09-03 16:28:15 -0300

    delete filial

--- routes/rotaFilial.js
+++ routes/rotaFilial.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { registrarFilial, listarFiliais, listarFilial, listarEmpresaComFiliais } = require("../controllers/filialController");
+const { registrarFilial, listarFiliais, listarFilial, listarEmpresaComFiliais, deletarFilial } = require("../controllers/filialController");
 const fililRoute = express.Router();
 const authMiddleware = require("../config/authMiddleware")
 
@@ -7,6 +7,7 @@ const authMiddleware = require("../config/authMiddleware")
 fililRoute.post('/registrar-filial', authMiddleware, registrarFilial);
 fililRoute.get('/listar-filiais', authMiddleware, listarFiliais);
 fililRoute.get('/:e_id/listar-filial', authMiddleware, listarFilial);
-fililRoute.get('/:e_id/listar-empresas-filiais', listarEmpresaComFiliais)
+fililRoute.get('/:e_id/listar-empresas-filiais', authMiddleware, listarEmpresaComFiliais);
+fililRoute.delete('/:f_id/deletar-filial', authMiddleware, deletarFilial)
 
 module.exports = fililRoute;
