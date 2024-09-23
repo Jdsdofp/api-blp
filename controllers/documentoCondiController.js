a453807fd038b93bd23bb101b2c6c58dadce1ebf
Jds Dofp <jdsdofp@gmail.com>
2024-09-23 18:01:19 -0300

    aajjaioik

--- controllers/documentoCondiController.js
+++ controllers/documentoCondiController.js
@@ -1,4 +1,6 @@
 const DocumentoCondicionante = require("../models/Documento_Condicionante");
+const Documento = require("../models/Documentos");
+const Filial = require("../models/Filial");
   
   
 module.exports.listarDocumentoCondicionantes = async (req, res)=>{
@@ -10,6 +12,36 @@ module.exports.listarDocumentoCondicionantes = async (req, res)=>{
         res.status(400).json(error);
     }
 }
+
+//Listar documento condicionante por filial
+module.exports.listarDocumentoCondicionanteFiliais = async (req, res) => {
+    try {
+        const documentoCondicionante = await DocumentoCondicionante.findAll({
+            include: [
+                {
+                    model: Documento,
+                    as: 'documento', // Usar o alias 'documento' definido no belongsTo
+                    attributes: ['d_filial_id'], // Inclui o ID da filial relacionado ao documento
+                    include: [
+                        {
+                            model: Filial,
+                            as: 'filiais', // Usar o alias 'filiais' definido no belongsTo no Documento
+                            attributes: ['f_nome', 'f_cnpj', 'f_cidade', 'f_uf'], // Pegue os dados da filial
+                        }
+                    ]
+                }
+            ]
+        });
+
+        res.status(200).json(documentoCondicionante);
+    } catch (error) {
+        console.error(error);
+        res.status(400).json(error);
+    }
+};
+
+  
+  
 
 module.exports.listarDocumentoCondicionante = async (req, res)=>{
 
--- routes/rotaDocumentoCondicionante.js
+++ routes/rotaDocumentoCondicionante.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { listarDocumentoCondicionantes, listarDocumentoCondicionante } = require("../controllers/documentoCondiController");
+const { listarDocumentoCondicionantes, listarDocumentoCondicionante, listarDocumentoCondicionanteFiliais } = require("../controllers/documentoCondiController");
 const documentoCondRoute = express.Router();
 const authMiddleware = require("../config/authMiddleware");
 
 
 documentoCondRoute.get('/listar-documento-condicionantes', authMiddleware, listarDocumentoCondicionantes);
+documentoCondRoute.get('/listar-documento-condicionantes-filiais', listarDocumentoCondicionanteFiliais);
 documentoCondRoute.get('/listar-documento-condicionante/:dc_id', authMiddleware, listarDocumentoCondicionante);
 
 module.exports = documentoCondRoute;
