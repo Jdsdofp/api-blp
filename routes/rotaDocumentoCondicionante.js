c264c315cf6de951f8dde45fd757e358afa830f7
Jds Dofp <jdsdofp@gmail.com>
2024-10-10 15:18:49 -0300

    fazendo a integração das atribuiçoes de usuarios

--- routes/rotaDocumentoCondicionante.js
+++ routes/rotaDocumentoCondicionante.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { listarDocumentoCondicionantes, listarDocumentoCondicionante, listarDocumentoCondicionanteFiliais, fecharCondicionante } = require("../controllers/documentoCondiController");
+const { listarDocumentoCondicionantes, listarDocumentoCondicionante, listarDocumentoCondicionanteFiliais, fecharCondicionante, atribuirUsuariosCondicao, listarDocumentoCond } = require("../controllers/documentoCondiController");
 const documentoCondRoute = express.Router();
 const authMiddleware = require("../config/authMiddleware");
 
@@ -7,6 +7,8 @@ const authMiddleware = require("../config/authMiddleware");
 documentoCondRoute.get('/listar-documento-condicionantes', authMiddleware, listarDocumentoCondicionantes);
 documentoCondRoute.get('/listar-documento-condicionantes-filiais', listarDocumentoCondicionanteFiliais);
 documentoCondRoute.get('/listar-documento-condicionante/:dc_id', authMiddleware, listarDocumentoCondicionante);
+documentoCondRoute.get('/listar-condicionante/:dc_id', authMiddleware, listarDocumentoCond)
 documentoCondRoute.put('/fechar-condicionante/:dc_id', authMiddleware, fecharCondicionante);
+documentoCondRoute.patch('/atribuir-usuarios-condicao/:dc_id', authMiddleware, atribuirUsuariosCondicao);
 
 module.exports = documentoCondRoute;
