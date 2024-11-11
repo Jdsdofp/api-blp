de5a2086eb35c45f445f307c7eefaf93bafa33cd
Jds Dofp <jdsdofp@gmail.com>
2024-11-11 18:06:35 -0300

    jauajsoko

--- routes/rotaCondicionantes.js
+++ routes/rotaCondicionantes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { registrarCondicionante, listarCondicionantes } = require("../controllers/condicionanteController");
+const { registrarCondicionante, listarCondicionantes, listarCondicionante } = require("../controllers/condicionanteController");
 const condicionanteRoute = express.Router();
 const authMiddleware = require("../config/authMiddleware");
 
 condicionanteRoute.post('/registrar-condicionante', authMiddleware, registrarCondicionante);
 condicionanteRoute.get('/listar-condicionantes', authMiddleware, listarCondicionantes);
+condicionanteRoute.get('/listar-condicionante/:c_id', authMiddleware, listarCondicionante);
 
 
 module.exports = condicionanteRoute;
