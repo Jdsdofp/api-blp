d68dbe1fdb4b92686ce7b3cd8ef930fe4df8fe7c
Jds Dofp <jdsdofp@gmail.com>
2024-11-21 17:58:27 -0300

    ajustando o controller debidos

--- controllers/debitosDocumentosController.js
+++ controllers/debitosDocumentosController.js
@@ -1,10 +1,19 @@
+const Documento = require("../models/Documentos");
 
 
 
 module.exports.registrarCusto = async (req, res) =>{
     try {
+        const {d_id} = req.params;
+        console.log('ID doc recebido', d_id);
+
+        const doc = await Documento.findByPk(d_id);
+        console.log('Doc encontrado', doc?.dataValues);
+
         
+
+
     } catch (error) {
-        
+        console.log('Log de Erro ao registrar Documento', error)
     }
 }
